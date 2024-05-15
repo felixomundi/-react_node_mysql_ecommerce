7db13423144613ba62d78c2f5981739141dac60d
Felix Omundi <91674030+felixomundi@users.noreply.github.com>
2024-05-15 15:40:50 +0300

    first commit

--- backend/src/controllers/mpesaController.js
+++ backend/src/controllers/mpesaController.js
@@ -1,21 +1,5 @@
 require("dotenv").config();
-const { default: axios } = require("axios");
 const MpesaService = require("../services/MpesaService");
-const ngrok = require("ngrok");
-async function ngrokConnect(){
-    try {
-        var url = await ngrok.connect({
-            proto: 'http', 
-            addr: 5000,
-            authtoken:process.env.AUTH_TOKEN,
-           });
-           console.log(url)
-    } catch (error) {
-        console.log(error);
-    }
-
-} ngrokConnect();
-
 const generateToken = async(req, res, next) => {    
    try {       
     const response = await MpesaService.generateTokenService();
@@ -59,19 +43,53 @@ const stkPush = async (req, res) => {
 }
 const callBack = async(req, res) => {    
     try {
-        // const callBackData = req.body.Body.stkCallback;
-        // console.log(callBackData);       
-            // const data = callBackData.Body.stkCallback.CallbackMetadata;           
-            // const amount = data.Item[0].Value;
-            // const transaction = data.Item[1].Value;
-            // const balance = data.Item[2].Value;
-            // const date = data.Item[3].Value;        
-            // const phone = data.Item[4].Value;  
-            
+        const callbackData = req.body;
+        const result_code = callbackData.Body.stkCallback.ResultCode;
+        if (result_code !== 0) {
+            const CheckoutRequestID = callbackData.Body.stkCallback.CheckoutRequestID;
+            const payment = await Payment.findOne({
+                where: {CheckoutRequestID}
+            });
+            if (payment) {
+                await payment.update({
+                    ResultDesc: callbackData?.Body?.stkCallback?.ResultDesc,
+                    status:'Failed',
+                });
+            }
+            return;
+        } else {
+
+            const CheckoutRequestID = callbackData.Body.stkCallback.CheckoutRequestID;
+            const payment = await Payment.findOne({
+                where: { CheckoutRequestID }
+            });
+
+            if (!payment) { }
+            // If the result code is 0, the transaction was completed
+            const body = await req?.body?.Body?.stkCallback?.CallbackMetadata;
         
+            // Get amount
+            const amountObj = await body.Item.find(obj => obj.Name === 'Amount');
+            const amount = await amountObj.Value;
         
+            // Get Mpesa code
+            const codeObj = await body.Item.find(obj => obj.Name === 'MpesaReceiptNumber');
+            const mpesaCode = await codeObj.Value;
+        
+            // Get phone number
+            const phoneNumberObj = await body.Item.find(obj => obj.Name === 'PhoneNumber');
+            const phone = await phoneNumberObj.Value;
+
+            await payment.update({
+                "ResultDesc":await callbackData?.Body?.stkCallback?.ResultDesc,
+                "MpesaReceiptNumber":mpesaCode,
+                "status":"Paid"
+            });            
+        }
+       
     } catch (error) {
-        return res.status(500).json(error.message);
+        // return res.status(500).json(error.message);
+        console.log(error.response);
     }
 }
 const stkPushStatus = async(req, res)=>{
